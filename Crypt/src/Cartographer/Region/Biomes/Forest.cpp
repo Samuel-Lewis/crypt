e21a9f050e19604337d2700b44c5ae3678415d5d
Jacob Gonzalez <jacobgonzalez5252@hotmail.com>
2015-10-16 13:43:32 +1030

    bushy

--- Crypt/src/Cartographer/Region/Biomes/Forest.cpp
+++ Crypt/src/Cartographer/Region/Biomes/Forest.cpp
@@ -14,7 +14,7 @@ Forest::Forest(int newWidth, int newHeight): Region(newWidth, newHeight, "Forest
 {
 	// Setting options for CellAuto
 	// height, width, iterations, alive chance, deathLimit, birthLimit, borders are walls
-	CellAuto::Options opt(height(),width(),3,0.4,4,4,false);
+	CellAuto::Options opt(height(),width(),2,0.45,4,4,false);
 
 	TILEGRID map = CellAuto::generate("Tree","Grass", getRegionName(),opt);
 
