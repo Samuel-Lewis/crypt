0942758c5d2636d766dff3dadf8379b0d891cc61
Samuel-Lewis <mazuel@gmail.com>
2015-10-26 23:37:35 +1030

    * Made plains actually plain-y

--- Crypt/src/Cartographer/Region/Biomes/Plains.cpp
+++ Crypt/src/Cartographer/Region/Biomes/Plains.cpp
@@ -10,7 +10,7 @@
 #include "../Features/Buildings/House.h"
 
 Plains::Plains() {}
-Plains::Plains(int newWidth, int newHeight): Region(newWidth, newHeight, "Plains")
+Plains::Plains(int newWidth, int newHeight, float density): Region(newWidth, newHeight, density, "Plains")
 {
 	// Set standard background tile, with random chance of ocassional tree
 	for (int x = 0; x < width(); x++)
@@ -24,32 +24,7 @@ Plains::Plains(int newWidth, int newHeight): Region(newWidth, newHeight, "Plains
 				replace(x,y, new Tile("Grass", false, getRegionName()));
 			}
 
-            // start a farm
-            if (lbRNG::normDist(0.0, 1.0) > 2.6)
-            {
-                replace(x, y, new Tile("Field", true, getRegionName()));
-            }
-
-            //continue a farm
-            //todo : replace with perlin noise
-            if (lbRNG::normDist(0.0, 1.0) > 0.2
-                    && (getTileAt(x > 1 ? x-1 : x, y)->getName() == "Field"
-                    || getTileAt(x, y > 1 ? y-1 : y)->getName() == "Field"))
-            {
-                replace(x, y, new Tile("Field", true, getRegionName()));
-            }
 		}
-	}
-
-
-	// Chuck in a house or two for good measure
-	for (int i = 0; i < lbRNG::normDist(2,1,0,4); i++)
-	{
-		House* house = new House;
-		// Keep trying to add the house
-		while (!replace(lbRNG::linear(0,width() - house->width()), lbRNG::linear(0,height() - house->height()), house, false)) {};
-		_features.push_back(house);
-		
 	}
 
 	INFO("Generated Plains");
