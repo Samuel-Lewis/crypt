fafa98e126b98d0b9b630f746a6ac501ee87bc22
Jacob Gonzalez <jacobgonzalez5252@hotmail.com>
2015-11-16 18:15:44 +1030

    minimap

--- Crypt/src/Core/GameController.cpp
+++ Crypt/src/Core/GameController.cpp
@@ -78,7 +78,7 @@ void GameController::draw()
 
     // draw minimap
 
-    if (!(player.screenPos.x > view.getSize().x * 0.75 - TILESIZE
+    if (!(player.screenPos.x > view.getSize().x * 0.75 - 2*TILESIZE
           && player.screenPos.y < view.getSize().y * 0.25 + TILESIZE))
     {
         window->setView(minimap);
